9586a96d88542026ff741fc766217dfb7632f1ec
Dhruv Patel <dhruvpatel10890@gmail.com>
2023-09-04 14:24:29 -0400

    modified db.js to secure private detials

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.s4kpw82.mongodb.net/AS2"
+  "mongodb+srv://yourUsername:yourPassword@cluster0.s4kpw82.mongodb.net/AS2"
 );
 mongoose.connection.on("connected", function () {
   console.log("Application is connected to Databse");
